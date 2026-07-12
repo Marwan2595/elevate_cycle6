3acba9ff003495efd4aa477d0458a9b471b349fe
Marwan Sayed <marwan.sayed.soliman95@gmail.com>
2026-07-13 00:07:13 +0300

    Register Endpoint

--- lib/core/constants/endpoints.dart
+++ lib/core/constants/endpoints.dart
@@ -2,4 +2,5 @@ class Endpoints {
   Endpoints._();
   static const String baseUrl = 'https://ecommerce.routemisr.com/api/v1';
   static const String getProductsEndpoint = '/products';
+  static const String registerEndpoint = '/register';
 }
